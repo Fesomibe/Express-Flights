510650d075c14c527751411ac49aa83bb0aa7f88
Fesomibe <francisesomibe1@gmail.com>
2024-01-10 15:26:02 -0600

    Initial update

--- routes/flights.js
+++ routes/flights.js
@@ -5,8 +5,12 @@ const flightsCtrl = require('../controllers/flights');
 
 router.get('/new', flightsCtrl.new);
 
+// GET /flights/:id
+router.get('/:id', flightsCtrl.show)
+
 router.post('/', flightsCtrl.create);
 
 router.get('/', flightsCtrl.index);
+
 
 module.exports = router;
